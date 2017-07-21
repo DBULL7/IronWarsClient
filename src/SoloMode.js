8ee6d88d778f20878f8cf6831631397a58251ecc
Devon Bull <dbull@live.com>
2017-07-21 17:42:06 -0600

    solomode component made, working on ui

---
+++ src/SoloMode.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class SoloMode extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  getCode(e) {
+    if(!e) return 
+    let text = e.target.innerText
+    this.setState({text: text})
+  }
+
+  make() {
+    if (!this.state.text) return 
+    const script = document.createElement("script");
+    script.append(this.state.text)
+    let placeToAppend = document.getElementById('code')
+    placeToAppend.append( script );
+  }
+
+  render() {
+    return (
+      <div className="App">
+         <div id="terminal" onKeyUp={(e) => {this.getCode(e)}} contentEditable={true}></div>
+        <button onClick={() => this.make()}>Create</button> 
+        <pre id="code"></pre>                
+      </div>
+    );
+  }  
+}
+
+export default SoloMode
